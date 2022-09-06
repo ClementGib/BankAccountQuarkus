c26d5484b833b1e3683909e176c7ca84c9d3556e
CodingClem <c.gibertpro@gmail.com>
2022-09-06 23:18:52 +0200

    feature#5: TDD BankAccountService tests description with quarkus mocking

--- quarkus-backend/domain/src/main/java/com/cdx/bas/domain/bank/account/BankAccountService.java
+++ quarkus-backend/domain/src/main/java/com/cdx/bas/domain/bank/account/BankAccountService.java
@@ -2,6 +2,13 @@ package com.cdx.bas.domain.bank.account;
 
 import com.cdx.bas.domain.bank.transaction.Transaction;
 
+
+/**
+ * Make a deposit from a transaction
+ * 
+ * @param deposit transaction
+ * @return transaction processing status
+ */
 public interface BankAccountService {
     public Transaction deposit(Transaction transaction);
 }
--- quarkus-backend/domain/src/main/java/com/cdx/bas/domain/bank/transaction/TransactionServiceImpl.java
+++ quarkus-backend/domain/src/main/java/com/cdx/bas/domain/bank/transaction/TransactionServiceImpl.java
@@ -11,7 +11,7 @@ import org.slf4j.LoggerFactory;
 @RequestScoped
 public class TransactionServiceImpl implements TransactionService {
 
-    private static Logger logger = LoggerFactory.getLogger(TransactionServiceImpl.class);
+    private static final Logger logger = LoggerFactory.getLogger(TransactionServiceImpl.class);
     
     @Inject
     BankAccountService bankAccountService;
